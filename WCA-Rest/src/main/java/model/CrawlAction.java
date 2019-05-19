c75b3f0250b5a80842a8f8e2b6072d167ae6adfe
Marina <marina.tzenkova@cytosmart.com>
2019-05-19 11:19:03 +0200

    adding javadoc for some classes

---
+++ WCA-Rest/src/main/java/model/CrawlAction.java
@@ -0,0 +1,10 @@
+package model;
+
+/** Represents data of last crawling action.
+ * This class takes care of storing number of pages visited,
+ * number of unique pages visited, time elapsed and
+ * also search depth.
+ * @version 0.1
+ */
+public class CrawlAction {
+}
