00ffd36e025662b4f34ffd9cfb338646c2bc50a2
Marina <marina.tzenkova@cytosmart.com>
2019-06-10 14:54:57 +0200

    will probably break everything :)

---
+++ src/main/java/model/Movie.java
@@ -0,0 +1,24 @@
+package model;
+
+/** Class that inherits from the Item class.
+ * Used for parsing of Movie generated data.
+ * Inherits all properties of parent class and has
+ * custom Artist property.
+ */
+public class Movie {
+    /**
+     * The director of the movie.
+     */
+    public String Director;
+
+    /**
+     * Field containing information about the writers
+     * of the movie.
+     */
+    public String Writers;
+    /**
+     * Field containing information about the stars
+     * participating in the movie.
+     */
+    public int Stars;
+}
---
+++ src/main/java/rest/Scraper.java
@@ -0,0 +1,37 @@
+package rest;
+
+import model.Item;
+import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Scraper is the main class for handling Jsoup scraping logic.
+ * It contains an instance of Parser class which is used for parsing html content into corresponding Item class.
+ * @version 0.1
+ */
+public class Scraper {
+    /**
+     * Object to convert scraped data to objects
+     */
+    public Parser parser;
+
+    /**
+     * Get a page's content
+     * @param url - page's url
+     * @return html content of page
+     */
+    public Document getPage(String url) {
+        return null;
+    }
+
+    /**
+     * Scrape data form a html page
+     * @param htmlContent - html to scrape
+     * @return list of scraped items
+     */
+    public List<Item> scrapeData(String htmlContent) {
+        return new ArrayList<>();
+    }
+}
