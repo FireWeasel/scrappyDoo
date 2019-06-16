0fdf8303d3f5fb09b1feda6990aa56c71dd5f6df
Lyubomir Yankov <ahhak123@gmail.com>
2019-06-16 17:56:40 +0200

    verifyExceptionIsNotThrownWhenGetSpecificDataIsCalledWithoutTypeParameter

--- src/test/java/rest/CrawlerTest.java
+++ src/test/java/rest/CrawlerTest.java
@@ -147,8 +147,16 @@ public class CrawlerTest {
      * parameter for item type is not passed to the function.
      */
     @Test
-    public void verifyExceptionIsNotThrownWhenGetSpecificDataIsCalledWithoutTypeParameter() {
+    public void verifyExceptionIsNotThrownWhenGetSpecificDataIsCalledWithoutTypeParameter() throws Exception {
+        Crawler crawler = new Crawler();
+        Scraper scraper = mock(Scraper.class);
+        List<Item> items = new ArrayList<Item>();
+        items.add(new Book());
+        when(scraper.scrapeData("")).thenReturn(items);
+        PowerMockito.whenNew(Scraper.class).withAnyArguments().thenReturn(scraper);
+        Item i = crawler.getSpecificData(null, "");
 
+        Assert.assertEquals(items.get(0), i);
     }
 
     /**
