f8fd28eb5e436139403e85e2355bb80ac0c63a57
Marina <marina.tzenkova@cytosmart.com>
2019-06-16 14:11:08 +0200

    Adding jersey and implementing endpoint signature

--- src/main/java/rest/CrawlService.java
+++ src/main/java/rest/CrawlService.java
@@ -2,6 +2,11 @@ package rest;
 
 import model.CrawlAction;
 
+import javax.inject.Singleton;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.xml.ws.Response;
 import java.util.List;
 
@@ -9,6 +14,8 @@ import java.util.List;
  * Crawler service for crawling a website, finding data and getting the last crawled action
  * @version 0.1
  */
+@Singleton
+@Path("/")
 public class CrawlService {
     /**
      * Last crawl actions executed
@@ -20,11 +27,16 @@ public class CrawlService {
      */
     public Crawler crawler;
 
+    public CrawlService(){}
+
     /**
      * Crawl an entire website
      * @param baseUrl website to crawl
      * @return a JSON response containing scraped data or empty string
      */
+    @GET
+    @Path("wholepage")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response crawlWholeWebsite(String baseUrl) {
         return  null;
     }
@@ -36,6 +48,9 @@ public class CrawlService {
      * @param keyword - filter by which to look for a specific item
      * @return a JSON response containing scraped data or empty string
      */
+    @GET
+    @Path("finddata")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response findData(String baseUrl, String type, String keyword) {
         return  null;
     }
@@ -44,6 +59,9 @@ public class CrawlService {
      * Get the last crawled action
      * @return a JSON response containing scraped data or empty string
      */
+    @GET
+    @Path("lastaction")
+    @Produces(MediaType.APPLICATION_JSON)
     public Response getLastCrawlingAction() {
         return null;
     }
