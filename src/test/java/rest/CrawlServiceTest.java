ed19d2e136a43f8fce85cb65d04c6ed260493586
Marina <marina.tzenkova@cytosmart.com>
2019-06-16 16:45:55 +0200

    Adding tests which validate if URL is valid and if a correct response format is returned
    Tests which check if exception is thrown when url does not contain http:// and also if response contains id and time and expected movies and books

--- src/test/java/rest/CrawlServiceTest.java
+++ src/test/java/rest/CrawlServiceTest.java
@@ -1,5 +1,8 @@
 package rest;
 
+import model.Item;
+import model.Movie;
+import model.Music;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,9 +10,22 @@ import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+import javax.ws.rs.core.Response;
+import java.awt.print.Book;
+import java.io.StringReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.Mockito.atLeastOnce;
 import static org.mockito.Mockito.verify;
 import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest({CrawlService.class})
@@ -41,6 +57,43 @@ public class CrawlServiceTest {
         //ASSERT
         PowerMockito.verifyNew(Crawler.class).withArguments(domain);
         verify(crawlMock, atLeastOnce()).getAllData(baseUri);
+    }
+
+    @Test
+    public void assertCorrectResponseFormatIsReturnedWhenCrawlingWholeWebsite() throws Exception {
+        List<String> stars = new ArrayList<String>(Arrays.asList("Tom Hanks", "Rebecca Williams", "Sally Field", "Michael Conner Humphreys"));
+        List<String> writers = new ArrayList<String>(Arrays.asList("Winston Groom", "Eric Roth"));
+        List<Item> expectedListOfItems = Arrays.asList(new Movie("Forrest Gump","Robert Zemeckis", "Drama",
+                "DVD", 1994, writers,stars),new Music("Clasical", "CD" , 2012, "Symphony","Ludwig van Beethoven"));
+
+        JsonObject expectedResponseJson = Json.createObjectBuilder()
+                .add("id", 0)
+                .add("time", "doesn't matter")
+                .add("movies", expectedListOfItems.stream().filter(i -> i.getClass().equals(Movie.class)).map(e -> e.toString()).reduce("", String::concat))
+                .add("music", expectedListOfItems.stream().filter(i -> i.getClass().equals(Music.class)).map(e -> e.toString()).reduce("", String::concat))
+                .add("books", expectedListOfItems.stream().filter(i -> i.getClass().equals(Book.class)).map(e -> e.toString()).reduce("", String::concat))
+                .build();
+        PowerMockito.whenNew(Crawler.class).withArguments(domain).thenReturn(crawlMock);
+        when(crawlMock.getAllData(baseUri)).thenReturn(expectedListOfItems);
+
+        Response response = crawlService.crawlWholeWebsite(baseUri);
+        JsonReader jsonReader = Json.createReader(new StringReader(response.getEntity().toString()));
+        JsonObject returnedJsonResponse = jsonReader.readObject();
+        jsonReader.close();
+
+        assertThat(expectedResponseJson, equalTo(returnedJsonResponse));
+    }
+    /**
+     * Test function that checks if an
+     * Exception is thrown when an invalid url
+     * is passed.
+     */
+    @Test(expected = Exception.class)
+    public void exceptionIsThrownWhenInvalidUrlIsPassedToCrawlWholeWebsite() throws Exception {
+        baseUri = "verywrongurl";
+        domain = "";
+
+        crawlService.crawlWholeWebsite(baseUri);
     }
 
     /**
@@ -94,13 +147,7 @@ public class CrawlServiceTest {
      */
     @Test
     public void emptyKeywordsArePassedForFindDataAndItemIsReturned(){}
-    /**
-     * Test function that checks if an
-     * Exception is thrown when an invalid url
-     * is passed.
-     */
-    @Test
-    public void exceptionIsThrownWhenInvalidUrlIsPassed(){}
+
     /**
      * Test function that checks if an
      * Exception is thrown when an invalid type
