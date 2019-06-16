f4b75d9bd7ac5e62f5239f266024c50996924304
Marina <marina.tzenkova@cytosmart.com>
2019-06-16 12:14:11 +0200

    Adding code implementation of illegalArgumentIsThrownWhenParameterIsNull
    Adding check if passed elements is null and if it is, throws IllegalArgumentException

--- src/main/java/rest/Parser.java
+++ src/main/java/rest/Parser.java
@@ -23,34 +23,37 @@ public class Parser {
      * @return parsed item
      */
     public Item parse(Elements elements) {
-        String type = "";
-        HashMap<String, String> item = new HashMap<>();
-        Item itemToReturn = null;
+        if(elements != null) {
+            String type = "";
+            HashMap<String, String> item = new HashMap<>();
+            Item itemToReturn = null;
 
 
-        if (elements.size() > 0) {
-            for (Element element : elements.select("table").select("tbody").select("tr")) {
-                if (element.select("th").text().equals("Category")) {
-                    type = element.select("td").text();
+            if (elements.size() > 0) {
+                for (Element element : elements.select("table").select("tbody").select("tr")) {
+                    if (element.select("th").text().equals("Category")) {
+                        type = element.select("td").text();
+                    }
+                    item.put(element.select("th").text(), element.select("td").text());
                 }
-                item.put(element.select("th").text(), element.select("td").text());
-            }
 
-            switch (type) {
-                case "Book":
-                    itemToReturn = parseBook(item);
-                    break;
-                case "Movie":
-                    itemToReturn = parseMovie(item);
-                    break;
-                case "Music":
-                    itemToReturn = parseMusic(item);
-                    break;
-                default:
-                    break;
+                switch (type) {
+                    case "Book":
+                        itemToReturn = parseBook(item);
+                        break;
+                    case "Movie":
+                        itemToReturn = parseMovie(item);
+                        break;
+                    case "Music":
+                        itemToReturn = parseMusic(item);
+                        break;
+                    default:
+                        break;
+                }
             }
+            return itemToReturn;
         }
-        return itemToReturn;
+        else throw new IllegalArgumentException("Elements must not be null!");
     }
 
     /**
